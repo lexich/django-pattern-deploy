617945946bbc48712d6728c4be67e9127fa2bfce
lexich <lexich121@gmail.com>
2012-08-21 17:56:44 +0400

    add virtualenv

--- django-pattern-deploy.py
+++ django-pattern-deploy.py
@@ -34,24 +34,46 @@ def find_path(filename):
             return file_path
     raise FileNotFoundException('File %s not found' % filename )
 
+
+def find_tool(name, paths):
+    for path in paths:
+	print path
+	path.append(name)
+	tool = os.path.join(*path)
+    	if os.path.exists(tool):
+            print("find_tool %s" % tool)
+            return tool
+    return name
+
+	
 def startproject(projectname):
     """
     wrapper to django-admin startproject $projectname
     raise FileNotFoundException
     """
     django_admin = find_path('django-admin.py')
+    python = 'python'
     subprocess.call([
-        'python', 
+        python, 
         django_admin, 
         'startproject', 
         projectname, 
         '--template={0}'.format(TEMPLATE_PROJECT_PATH)
         ])  
     print "Create projectname"
+    env = os.path.join(projectname,'.env')
     subprocess.call([
-	'pip','install','-r',
+	"virtualenv",
+	os.path.join(projectname,'.env')
+    ])
+    tool_path = [[env,"bin"],[env,"Script"]]
+    pip = find_tool("pip", tool_path)
+    python = find_tool("python", tool_path)
+    subprocess.call([
+	pip,'install','-r',
 	os.path.join(projectname,'requirements.txt')
     ])
+    return python
 
 
 
@@ -59,13 +81,14 @@ class Manage(object):
     """docstring for Manage 
         wrapper to manage.py
     """
-    def __init__(self, projectname):
+    def __init__(self, projectname,python='python'):
+        self.python = python
         self.manage = os.path.join('.',projectname,'manage.py')
         if not os.path.exists(self.manage):
             raise FileNotFoundException("File %s not found" % self.manage )
 
     def _m(self, *args):
-        params = ['python', self.manage ] 
+        params = [self.python, self.manage ] 
         for arg in args:
             params.append(arg)
         subprocess.call( params )
@@ -92,8 +115,9 @@ def main(projectname):
     print "Clear project"
     rm_rf(projectname)
     try:
-        startproject(projectname)
-        manage = Manage(projectname)        
+        python = startproject(projectname)
+        print( "Python path %s" % python)
+        manage = Manage(projectname, python)        
         manage.syncdb()
         manage.migrate()
         manage.createsuperuser()
