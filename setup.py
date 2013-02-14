712003b2ee3e5bae17d4cf2c507c46ee673b47a7
Efremov Alex <lexich121@gmail.com>
2013-02-14 11:40:12 +0400

    0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="Django pattern deploy",
-	version="0.0.5",
+	version="0.0.6",
 	url="https://github.com/lexich/django-pattern-deploy",
 	author="lexich",
 	install_requires=[
